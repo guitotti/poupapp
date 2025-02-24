b77fa9db0df9b68e2a63cd06c263072f5fc643d2
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-02-24 18:56:54 -0300

    feat: create user slice

---
+++ src/redux/slices/usuarioSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  nome: "",
+  renda: "",
+  objetivoFinanceiro: "",
+};
+
+const usuarioSlice = createSlice({
+  name: "usuario",
+  initialState,
+  reducers: {},
+});
+
+export default usuarioSlice.reducer;
