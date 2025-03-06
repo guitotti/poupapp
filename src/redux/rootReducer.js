0c946c22f8e3431d54f09c81bc3137e4951c2e72
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-03-05 22:07:37 -0300

    feat: create transactions slice

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import usuarioSlice from "./slices/usuarioSlice";
 import objetivosTiposSlice from "./slices/objetivosTiposSlice";
+import transacoesSlice from "./slices/transacoesSlice";
 
 const rootReducer = combineReducers({
   usuario: usuarioSlice,
   objetivos: objetivosTiposSlice,
+  transacoes: transacoesSlice,
 });
 
 export default rootReducer;
