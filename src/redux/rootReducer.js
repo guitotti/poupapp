6043fbcf9ea18e2e5c90cf4bac22660d5570c857
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-03-06 23:23:20 -0300

    feat: create new action to add new bank accounts

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import usuarioSlice from "./slices/usuarioSlice";
 import objetivosTiposSlice from "./slices/objetivosTiposSlice";
 import transacoesSlice from "./slices/transacoesSlice";
+import contasSlice from "./slices/contasSlice";
 
 const rootReducer = combineReducers({
   usuario: usuarioSlice,
   objetivos: objetivosTiposSlice,
   transacoes: transacoesSlice,
+  contas: contasSlice,
 });
 
 export default rootReducer;
