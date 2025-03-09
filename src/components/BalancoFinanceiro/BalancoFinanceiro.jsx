4c9309099d9e6e2f2a10e20e94fe00994e133f14
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-03-09 12:57:31 -0300

    feat: spenses by categories using chart.js

--- src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
+++ src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
@@ -2,6 +2,27 @@ import Cartao from "../Cartao/Cartao";
 import CartaoCabecalho from "../Cartao/CartaoCabecalho/CartaoCabecalho";
 import CartaoCorpo from "../Cartao/CartaoCorpo/CartaoCorpo";
 import styled from "styled-components";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { useSelector } from "react-redux";
+import { gastosPorCategoria } from "src/redux/selectors/gastosPorCategoriaSelector";
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement
+);
 
 export const AreaChart = styled.div`
   padding: var(--padding-xs);
@@ -10,11 +31,48 @@ export const AreaChart = styled.div`
 `;
 
 const BalancoFinanceiro = () => {
+  const categoriasAgrupadas = useSelector(gastosPorCategoria);
+  const data = {
+    labels: Object.keys(categoriasAgrupadas),
+    datasets: [
+      {
+        label: "Gastos por categoria",
+        data: Object.values(categoriasAgrupadas),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+    },
+  };
+
   return (
     <Cartao>
       <CartaoCabecalho>Gastos por categoria</CartaoCabecalho>
       <CartaoCorpo>
-        <AreaChart></AreaChart>
+        <AreaChart>
+          <Pie data={data} options={options} />
+        </AreaChart>
       </CartaoCorpo>
     </Cartao>
   );
